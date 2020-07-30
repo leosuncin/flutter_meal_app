449aa3b5c50d8559a29023887cbd5a5be71c294c
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-07-29 19:48:48 -0600

    Show meals by category

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './dummy_data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/categories-meals';
 
@@ -7,13 +9,19 @@ class CategoryMealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final routeArgs =
         ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(routeArgs['id']))
+        .toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(routeArgs['title']),
       ),
-      body: Center(
-        child: Text('The recipes for the category'),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
